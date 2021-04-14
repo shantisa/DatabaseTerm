76dce63c72fa690d8c9d6018217703198c8bd521
Komal Patel <kap52644@uga.edu>
2021-04-13 20:33:54 -0400

    init commit

---
+++ src/test/java/edu/cs/uga/DatabaseTermProject/DatabaseTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cs.uga.DatabaseTermProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseTermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
